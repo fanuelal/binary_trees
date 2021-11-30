ef757d8edc2b850baee6671c8d9978748dc0ce7c
fanuelal <fanuelalmawstop.com>
2021-11-30 10:29:04 -0800

    is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - leaf checker
+ *@node: node given
+ *Return: 1 or 0
+ **/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if(!node)
+return (0);
+if(!node->right && !node->left)
+return (1);
+return (0);
+}
