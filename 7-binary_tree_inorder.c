06bb0101d780273225b5795182a07af64c469b18
fanuelal <fanuelalmawstop.com>
2021-11-30 12:00:22 -0800

    inorder

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ *binary_tree_inorder - postorder orderer
+ *@tree: tree to be ordered
+ *@func: pointer function to display
  **/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (!tree || !func)
-  return;
+return;
 binary_tree_inorder(tree->left, func);
 func(tree->n);
 binary_tree_inorder(tree->right, func);
