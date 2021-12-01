3db22fc45a7b4e6b4a384487e54210b42aa99692
fanuelal <fanuelalmawstop.com>
2021-12-01 12:30:32 -0800

    node count

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - count nodes in a tree with children
+ * @tree: tree to check
+ * Return: number of nodes with child
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+size_t node = 0;
+if(!tree)
+return (0);
+if (tree->left || tree->right)
+{
+node += 1;
+node += binary_tree_nodes(tree->right);
+node += binary_tree_nodes(tree->left);
+} 
+return (node);
+}
