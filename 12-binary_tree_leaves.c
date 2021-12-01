923f97fe4cddf968d8bf2da15d0c8480306af70d
fanuelal <fanuelalmawstop.com>
2021-12-01 11:34:24 -0800

    leaf counter

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,10 +3,10 @@
  * binary_tree_leaves - counter of leaves in a tree
  * @tree: tree to check
  * Return: number of leaves
- */
+ **/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  size_t l = 0, r = 0, leaf = 0;
+size_t l = 0, r = 0, leaf = 0;
 if (!tree)
 return (0);
 l = binary_tree_leaves(tree->left);
