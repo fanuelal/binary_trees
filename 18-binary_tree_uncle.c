6eadb4a0ddc1639f6553a74f8f260eaf349348bc
fanuelal <fanuelalmawstop.com>
2021-12-02 12:51:42 -0800

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,18 +8,24 @@
  **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (!node || !node->parent->parent)
-return (NULL);
-if (node->parent != node->parent->parent->right)
-{
-if (!node->parent->parent->right)
-return (NULL);
-return (node->parent->parent->right);
+	if (!node || !node->parent)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
 }
-else
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: pointer to the sibling node
+ **/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-if (!node->parent->parent->left)
-return (NULL);
-return (node->parent->parent->left);
-}
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
