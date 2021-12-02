a4bf86a7fb535b39040f65f1c660a04643b8d92c
fanuelal <fanuelalmawstop.com>
2021-12-02 11:14:25 -0800

    sibling

--- binary_trees.h
+++ binary_trees.h
@@ -40,5 +40,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int powH(int x);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif
